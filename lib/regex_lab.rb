691d78ad8f10088c765cf21d22cb0654051cabde
Danny Soto <87dsoto@gmail.com>
2020-01-24 05:12:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,19 @@
-def starts_with_a_vowel?(word)
 
+def starts_with_a_vowel?(word)
+  regex = /^[aeiouAEIOU]/
+  if word.match(regex) == nil 
+    return false 
+  else 
+    return true 
+  end 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
